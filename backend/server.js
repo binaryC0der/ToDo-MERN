530a0d70c3a7816d403ded0c1ec600a6f94f8a2f
sg0808 <saksham.goel0808@gmail.com>
2023-03-01 18:32:45 +0530

    Create the react build

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const path = require("path");
 const express = require("express");
 const dotenv = require("dotenv");
 const colors = require("colors");
@@ -17,4 +18,13 @@ app.use(express.urlencoded({extended:false}));
 
 app.use("/api/goals",goalRoutes);
 app.use("/api/users",userRoutes);
+
+//Serve FrontEnd
+if(process.env.NODE_ENV==="production"){
+    app.use(express.static(path.join(__dirname,"../frontend/build")))
+    app.get("*",(req,res) => res.sendFile(path.resolve(__dirname,"../","frontend","build","index.html")))
+}
+else {
+app.get("/",(req,res) => res.send("Please set to production"));
+}
 app.use(errorHandler);
